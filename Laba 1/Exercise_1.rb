a2159c3b2ddf39c64fec82d092f9cb88554a6c68
Setet <72457086+Setet@users.noreply.github.com>
2022-02-02 17:47:31 +0300

    v1.01

--- Laba 1/Exercise_1.rb
+++ Laba 1/Exercise_1.rb
@@ -1,16 +1,20 @@
 # Знакомство с Ruby
+#1
 puts "Hello world!"
 
+#2
 puts "Wake the fuck up, #{ARGV[0]}!"
 
+#3
 puts "Какой твой любимый ЯП, #{ARGV[0]}?"
 answer = STDIN.gets.chomp
 if ["Руби","руби","Ruby","ruby",].include? answer.downcase
-	puts ")"
+	puts ":)"
 else
-	puts "("
+	puts ":("
 end
 
+#4
 puts "Введи команду Ruby, #{ARGV[0]} :"
 rub_com = STDIN.gets.chomp
 system "ruby -e \"#{rub_com}\""
