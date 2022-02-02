ab4b72ee47cca0edd99aee3c08335ecb637c5d46
Setet <72457086+Setet@users.noreply.github.com>
2022-02-02 18:15:52 +0300

    v1.01

--- Laba 1/Exercise_2.rb
+++ Laba 1/Exercise_2.rb
@@ -2,12 +2,12 @@
 #1
 def digits_sum(x)
 	q=10
-	res = 0
+	sum = 0
 	while x > 0
-		res += x % q
+		sum += x % q
 		x /= q
 	end
-	puts "Сумма = #{res}"
+	puts "Сумма = #{sum}"
 end
 
 #2
@@ -39,12 +39,33 @@ end
 
 def digits_op(x)
 	q=10
-	res = 1
+	op = 1
 	while x > 0
-		res *= x % q
+		op *= x % q
 		x /= q
 	end
-	puts "Произведение = #{res}"
+	puts "Произведение = #{op}"
+end
+
+#3
+def method_1(x)
+
+end
+
+def method_2(x)
+q=10
+	op = 1
+	while x > 0
+		if ((x % q)/5) < 1
+			op *= x % q
+		end
+		x /= q
+	end
+	puts "Произведение,не делящихся на 5 = #{op}"
+end
+
+def method_3(x)
+
 end
 
 def Working_with_numbers(x)
@@ -52,7 +73,10 @@ def Working_with_numbers(x)
 	#digits_sum(x)
 	#digits_max(x)
 	#digits_min(x)
-	digits_op(x)
+	#digits_op(x)
+	#method_1(x)
+	method_2(x)
+	#method_3(x)
 end
 
-Working_with_numbers(1234)
+Working_with_numbers(55523)
