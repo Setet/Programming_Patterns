d4c95d83837587f3a601395d8fc4e4f5a0fea5b6
Setet <72457086+Setet@users.noreply.github.com>
2022-02-16 00:08:51 +0300

    v1.02

--- Laba 1/Exercise_3.rb
+++ Laba 1/Exercise_3.rb
@@ -1,10 +1,10 @@
 # Список
 # 1
 def array_min(array)
-  min = 10
+  min = 100
   for item in array do
-    if min > item then
-      min = item
+    if min > item.to_i  then
+      min = item.to_i 
     end
   end
   puts "Мин элем = #{min}"
@@ -13,8 +13,8 @@ end
 def array_max(array)
   max = 0
   for item in array do
-    if max < item then
-      max = item
+    if max < item.to_i  then
+      max = item.to_i 
     end
   end
   puts "Макс элем = #{max}"
@@ -29,11 +29,11 @@ def array_sum(array)
 end
 
 def array_op(array)
-  op = 0
+  op = 1
   for item in array do
     op *= item.to_i
   end
-  puts "Произв элем = #{щз}"
+  puts "Произв элем = #{op}"
 end
 
 def file(q,path)
@@ -55,10 +55,10 @@ def clava(x)
   arr = [] 
   str = ''
   puts 'Вводите элементы списка: ' 
-  str = gets.chomp
+  str = STDIN.gets.chomp
   while str != ''
     arr.push str.to_i
-    str = gets.chomp
+    str = STDIN.gets.chomp
   end
   main(arr,x)
 end
@@ -66,35 +66,31 @@ end
 def main(array,x)
   case x
   when 1
+    puts "Ваш метод :> Минимальный элем "
     array_min(array)
   when 2
+    puts "Ваш метод :> Максимальный элем "
     array_max(array)
   when 3
+    puts "Ваш метод :> Сумма элементов "
     array_sum(array)
   when 4
+    puts "Ваш метод :> Произведение элементов "
     array_op(array)
   end
 end
 
 # 2
-puts "Список методов :"
-puts "1) Минимальная цифра числа\n"+
-"2) Максимальная цифра числа \n"+
-"3) Сумма цифр числа\n"+
-"4) Произведение цифр числа \n"
-puts "Выбирайте метод :>"
-x = gets.chomp.to_i
-
-puts "Откуда считать?"
-puts "1) Клава \n"+
-"2) Файл \n"
-y = gets.chomp.to_i
+x = ARGV[0].to_i
+y = ARGV[1].to_i
 
 case y
 when 1
+  puts "Считываем с клавы"
   clava(x)
 when 2 
+  puts "Считываем с файла"
   puts "Пропишите путь к файлу :>"
-  path = gets.chomp
+  path = STDIN.gets.chomp
   file(x,path)#внутри путь должен лежать
 end
