ad01463b27c13bdab398128bd04460d5b775d295
Setet <72457086+Setet@users.noreply.github.com>
2022-04-30 14:59:00 +0300

    v3.00

--- Laba 2/Department.rb
+++ Laba 2/Department.rb
@@ -1,76 +1,128 @@
+require_relative 'Post_list.rb'
+require_relative 'Post.rb'
+
 class Department
+    attr_accessor :name 
+    attr_reader :phone_number
 
-  attr_accessor :name
-  attr_reader :phone
-
-  def initialize (name, phone, *duty)
-  @name = name
-  self.phone = phone
-  @duty = duty
-  @index_duty = -1 # Индекс выбранной обязанности
-  end
-
-  # Конструктор, который кушает строку(вопросик по правильности работы)
-  def Department.read_line(line)
-    component = line.chomp.split(';')
-    new(component[0], component[1],component[2].split(','))
-  end
-  
-  # Получение информации
-  def to_s
-    "Название: #{@name};\nТелефон : #{@phone}; \nОбязанности : \n#{duty}\n"
-  end
-
-  # Вывод всех обязанностей
-  def duty
-    s = ""
-    @duty.each_index{|i| s += "#{i} - #{@duty[i]} \n"}
-    s
-  end
-
-  # Добавить обязанность
-  def duty_add(value)
-    @duty.append(value)
-  end
-
-  # Выделить обязанность
-  def duty_select(index)
-    @index = index
-  end
-
-  # Удалить обязанность
-  def duty_delete
-    @duty.delete_at(@index)
-    @index = -1
-  end
-
-  # Получить текст обязанности
-  def get_text_sel_duty
-    @duty[@index]
-  end
-
-  # Заменить текст выделенной обязанности
-  def change_text_sel_duty(value)
-    @duty[@index] = value
-  end
-
-  # Сэттэр
-  def phone=(value)
-    if self.class.verify_phone(value)
-      @phone = value
-    else raise ArgumentError.new("Номер телефона введен неверно!")
+    def initialize(name,phone_number,duties=Hash.new,selected_duty=nil,post_list=Post_list.new)
+        self.name= name
+        self.phone_number= phone_number
+        @duties = duties
+        @selected_duty = selected_duty
+        @post_list = post_list
     end
-  end
 
-  # Метод класса для проверки номера телефона
-  def self.verify_phone(phone)
-    /^((\+7|7|8)+([0-9]){10})$/.match(phone).to_s == phone
-  end
+    def phone_number=(phone_number)
+        if not Department.is_phone_number?(phone_number)
+            raise "Некорректный номер телефона"
+        end
+        phone_number.gsub!(/^8/, "7")
+        @phone_number = phone_number.delete("- )(+")
+    end
 
-  # Вывод всех обязанностей в txt
-  def duty_write_txt
-    s = ""
-    @duty.each_index{|i| s += "#{@duty[i]},"}
-    s.chop
-  end
+    # Получение информации
+    def to_s()
+        department_str="#{@name}|#{@phone_number}"
+        @duties.keys.each{|duty| department_str+="\n#{duty}:#{@duties[duty]}"}
+        department_str+= @post_list.to_s
+        department_str
+    end
+    
+    # Получение информации,но в урезанной форме
+    def to_s_short()
+        department_str="#{@name}|#{@phone_number}"
+    end
+    
+    # Вывод всех обязанностей
+    def Department.get_dep_str(dep)
+        dep = dep.split("\n")
+        dep_name,phone_number=dep[0].split("|")
+        duties=Hash.new
+        dep[1..dep.size].each do|duty| 
+            duty,spec = duty.split(":")
+            duties[duty]=spec
+        end
+        Department.new(dep_name,phone_number,duties)
+    end
+
+    # Добавить обязанность
+    def add_duty(duty,specification)
+        @duties[duty] = specification
+    end
+
+    # Выделить обязанность
+    def select_duty(duty)
+        if @duties.has_key?(duty)
+            @select_duty = duty
+        else
+            raise "Нет обязанности"
+        end
+    end
+
+    # Удалить обязанность
+    def delete_duty()
+        @duties.delete(@select_duty)
+        @select_duty = @duties.keys[0]
+    end
+
+     # Получить текст обязанности
+    def get_specification()
+        @duties[@select_duty]
+    end
+
+    # Заменить текст выделенной обязанности
+    def change_specification(specification)
+        @duties[@select_duty] = specification
+    end
+
+    # Получить обязанности
+    def get_duties()  
+        @duties.keys.join(" | ")
+    end
+
+    # Добавить должность
+    def add_post(post_name,salary,vacancy)
+        @post_list.add_note(Post.new(@name,post_name,salary,vacancy))
+    end
+
+    # Выбрать должность
+    def select_post(index)
+        @post_list.choose_note(index)
+    end
+
+    # Возвращает выбраную должность
+    def get_selected_post()
+        @post_list.get_note
+    end
+
+    # Удалить должность
+    def delete_post()
+        @post_list.delete_note
+    end
+
+
+    def change_post(post_name,salary,vacancy)
+        @post_list.change_note(Post.new(@name,post_name,salary,vacancy))
+    end
+
+    # Получить вакансию
+    def get_vacancies()
+        @post_list.get_vacancies
+    end
+
+    # Получить номер вакансий
+    def get_number_vacancies()
+        self.get_vacancies.length
+    end
+
+    # Получть все должности
+    def get_all_posts()
+        @post_list.post_list
+    end
+
+    # Метод класса для проверки номера телефона(переделал,она не моя)
+    def self.is_phone_number?(phone_number)
+        return phone_number.scan(/^(\s*)?(\+?7|8)([(\- ]?\d{3}[)\- ]?)(\d{3}[\- ]?\d{2}[\- ]?\d{2}|\d{2}[\- ]?\d{3}[\- ]?\d{2})(\s*)?$/).length == 1
+    end
 end
