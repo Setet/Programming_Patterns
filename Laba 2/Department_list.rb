e9b00b0b46d1022e0d3104f2b1a8bcd75c1f822e
Setet <72457086+Setet@users.noreply.github.com>
2022-04-30 14:58:36 +0300

    v2.00

--- Laba 2/Department_list.rb
+++ Laba 2/Department_list.rb
@@ -1,68 +1,94 @@
-path = File.dirname(__FILE__) # Получили путь к папке
-require "#{path}/Department.rb"
-
+require_relative 'Department.rb'
 class Department_list
 
-  # Конструктор
-  def initialize(list_departments)
-    @departments = list_departments
-    @index = -1
-  end
-
-  # Добавления записи
-  def add_note(department)
-    @departments.push(department)
-  end
-
-  # Выделяем запись
-  def choose_note(index)
-    if @departments.length > index and index >= 0 then
-      @index = index
-    else raise ArgumentError.new("Индекс вышел за пределы массива!")
+    def initialize(arr=Array.new())
+        @dep_list=arr
+        @selected_note=0
     end
-  end
 
-  # Заменяет запись
-  def change_note(department)
-    @departments[@index] = department
-  end
-
-  # Возвращает запись
-  def get_note
-    @departments[@index]
-  end
-
-  # Удалет запись
-  def delete_note
-    @departments.delete_at(@index)
-    @index = -1
-  end
-
-  def to_s
-    s = ""
-    @departments.each_index{|i| s += "Отдел - #{i}\n#{@departments[i]}\n"}
-    s
-  end
-
-  def Department_list.read_from_txt(file)
-    file = File.new(file, "r")
-    list_departments = [] 
-    for line in file.readlines
-      list_departments.push(Department.read_line(line))
+    # Добавления записи
+    def add_note(department)
+        @dep_list.append(department)
     end
-    file.close()
-    new(list_departments)
-  end
 
-
-  def write_to_txt(file)
-    File.open(file, "w") do |f|
-      @departments.each{|x| f.puts("#{x.name};#{x.phone};#{x.duty_write_txt}")}
+    # Выделяем запись
+    def choose_note(index)
+        @selected_note=index
     end
-  end
 
-  def Department_list.read_from_yaml(path) 
-    Department_list.new(Psych.safe_load_file(path, permitted_classes: [Department]))
-  end
+    # Заменяет запись
+    def change_note(department)
+        @dep_list[@selected_note]=department
+    end
+    
+    # Возвращает запись
+    def get_note()
+        @dep_list[@selected_note]
+    end
 
+    # Удалет запись
+    def delete_note()
+        @dep_list.delete_at(@selected_note)
+        @selected_note=0
+    end
+
+    # Читалка списка отдела через txt
+    def Department_list.read_from_txt(path)
+        file = File.new(path, "r:UTF-8")
+        text = file.read
+        arr = text.split((/\n_+\n/)).map do|dep|
+            Department.get_dep_str(dep)
+        end
+        Department_list.new(arr)
+    end
+    
+    # Запись в txt
+    def write_to_txt(path)
+        File.open("#{path}", "w") do |file|
+            @dep_list.each do |e|
+                file.puts e
+                file.puts "_"
+            end
+        end
+    end
+
+    # Запись в yaml
+    def write_to_yaml(path)
+        File.open(path, "w") do |file|
+            file.write(YAML.dump(@dep_list))
+        end
+    end
+    
+    # Читалка списка отдела через txt 
+    def Department_list.read_from_yaml(path) 
+        Department_list.new(Psych.safe_load_file(path, permitted_classes: [Department,Post_list,Post]))
+    end
+
+    # Сортировка списка отдела по имени
+    def sort_by_name()
+        @dep_list.sort_by!{|dep| dep.name.downcase}
+    end
+
+    # Сортировка по по количеству вакантных должностей
+    def sort_by_numbers_vacancies
+        @dep_list.sort_by{|dep| dep.get_number_vacancies}
+    end
+
+    # Получение информации
+    def to_s()
+        str=""
+        @dep_list.each do |dep|
+            str+=dep.to_s() +"\n"+"_\n"
+        end
+        str
+    end
+
+    # Получение информации,но в урезанной форме
+    def to_s_short()
+        str=""
+        @dep_list.each do |dep|
+            str+=dep.to_s_short() +"\n"+"_\n"
+        end
+        str
+    end
 end
