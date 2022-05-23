fbc9950a0a365a83b0714764df1b71f3fd4d2ba8
Setet <72457086+Setet@users.noreply.github.com>
2022-05-24 01:02:58 +0300

    Реализовал поиск объекта в файле по ключу+ начал писать комментарии

--- My personal Ruby project/Movies/Lib/movie_store.rb
+++ My personal Ruby project/Movies/Lib/movie_store.rb
@@ -2,16 +2,27 @@ require 'yaml/store'
 
 class MovieStore
 
+	# при инициализации созадём объект ямл стора для чтения/записи
+	# кушаем название файла кстати
 	def initialize(file_name)
 		@store = YAML::Store.new(file_name)
 	end
 
+	# метод возвращает нам объект movie по ключу
+	def find(id)
+		@store.transaction do
+			@store[id]
+		end
+	end
+
+	# метод для вывода названий всех фильмов что у нас есть в БД
 	def all
 		@store.transaction do
 			@store.roots.map { |id| @store[id] }
 		end
 	end
 
+	# метод для записи фильма
 	def save(movie)
 		@store.transaction do
 			unless movie.id
